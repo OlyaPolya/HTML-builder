5b25045f2027cc8f73ddda25d3435846b1ad850e
Olya_Polyanskaya <polyanskaya3000@gmail.com>
2022-05-19 22:38:30 +0700

    feat: finish 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,32 @@
+const fs = require('fs');
+const path = require('path');
+const copyFilePath = path.resolve(__dirname, 'files-copy');
+const originalFilePath = path.resolve(__dirname, 'files');
 
+async function copyDir() {
+  await fs.promises.mkdir(copyFilePath, { recursive: true }, (err) => {
+    if (err) throw err;
+  });
+
+  const data = await fs.promises.readdir(originalFilePath);
+
+  data.forEach((file) => {
+    fs.writeFile(path.join(copyFilePath, `${file.toString()}`), '', (err) => {
+      if (err) throw err;
+    });
+    fs.readFile(path.join(originalFilePath, `${file.toString()}`), 'utf-8', (err, data) => {
+      if (err) throw err;
+      fs.appendFile(path.join(copyFilePath, `${file.toString()}`), `${data}`, (err) => {
+        if (err) throw err;
+      });
+    });
+  });
+
+  console.log('Папка files-copy готова. Содержимое папки files скопировано в папку files-copy ');
+}
+
+try {
+  copyDir();
+} catch (err) {
+  console.error(err);
+}
